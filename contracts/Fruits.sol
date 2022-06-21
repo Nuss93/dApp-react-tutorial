0f57827ff31d94b14c5fd82f11f78f4752e6122f
Nuss93 <nasreenrzk@gmail.com>
2022-06-21 11:43:33 +0800

    FUCK ME

--- contracts/Fruits.sol
+++ contracts/Fruits.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma experimental ABIEncoderV2;
-pragma solidity >= 0.4.22 < 0.9.0;
+pragma solidity >=0.4.21 < 0.7.0;
 
 contract Fruits {
     string[] myFruits;
